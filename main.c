4ff866900096637423b8946b3621a7a26312a038
PennacchioDavide <dapennacchio@hotmail.com>
2024-12-23 10:46:35 +0100

    creating cell struct and print basic

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include <raylib.h>
+#include <stdlib.h>
+#include <raymath.h>
+#include <time.h>
+
+#define COLS 10
+
+#define ROWS 10
+
+const int screenWidth = 400;
+const int screenHeight = 400;
+
+const int cellWidth = screenWidth / COLS;
+const int cellHeight= screenHeight / ROWS;
+
+typedef struct Cell {
+    int i;
+    int j;
+}Cell;
+
+Cell grid[ROWS][COLS];
 
 int main() {
 
-    InitWindow(800, 450, "basic window"); // Window Initialisation
+    srand(time(0));
 
-    SetTargetFPS(144); // Setting FPS
+    InitWindow(screenWidth, screenHeight, "basic window"); // Window Initialisation
+
+    for (int i = 0; i < COLS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            grid[i][j].i = i;
+            grid[i][j].j = j;
+        }   
+    }
 
     while (!WindowShouldClose()) { // Game Loop
 
@@ -12,7 +39,11 @@ int main() {
 
             ClearBackground(RAYWHITE); // Background Color
 
-           
+            for (int i = 0; i < COLS; i++) {
+                for (int j = 0; j < ROWS; j++) {
+                    CellDraw(grid[i][j]);
+                }   
+            }
 
         EndDrawing();
     }
@@ -20,4 +51,8 @@ int main() {
     CloseWindow(); // WIndow Closing
 
     return 0;
+}
+
+void CellDraw(Cell cell) {
+    DrawRectangleLines(cell.i * cellWidth, cell.j * cellHeight, cellWidth, cellHeight, BLACK);
 }
