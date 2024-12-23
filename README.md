9d355a60d3b2cc75952aafe6e94a45b5ad67cc8c
PennacchioDavide <dapennacchio@hotmail.com>
2024-12-23 16:26:11 +0100

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is a simple Clone of The Classic Game Pong
+This is a simple Clone of The Classic Game Snake
 
 You can use the makefile to mount the project but you  need to have installed raylib before
--- main.c
+++ main.c
@@ -7,119 +7,16 @@ const int screenHeight = 720;
 
 int main() {
 
-    InitWindow(screenWidth, screenHeight, "basic window");
-    SetWindowTitle("Pong");
+    InitWindow(screenWidth, screenHeight, "Snake");
     HideCursor();
     SetTargetFPS(60);
 
-    Rectangle TopWhite = {10, 0, 1060, 10}; // Top White Line
-    Rectangle BotWhite = {10, 710, 1060, 10}; // Bot White Line
-    Rectangle LeftRed = {0, 0, 10, 720}; // Left Red Line
-    Rectangle RightRed = {1070, 0, 10, 720}; // Right Red Line
-
-    Rectangle LeftPaddle = {50, 310, 20, 120}; // Left Paddle
-    Rectangle RightPaddle = {1010, 310, 20, 120}; // Right Paddle
-
-    Vector2 ballPos = { 526.25, 351.25 }; // Circle Position
-    float ballRadius = 17.5; // Circle Radius
-    Vector2 ballSpeed = {5.0f, 4.0f}; // Ball Speed
-
-    bool winRight = false;
-    bool winLeft = false;
-
-    typedef struct Score {
-        int right;
-        int left;
-    }Score;
-     
-    Score score = {0, 0};
-    char scoreText[50];
-
     while (!WindowShouldClose()) {
-
-        // Input
-        if (IsKeyDown(KEY_UP) && !CheckCollisionRecs(RightPaddle, TopWhite)) {
-            RightPaddle.y -= 20;
-        }
-        if (IsKeyDown(KEY_DOWN) && !CheckCollisionRecs(RightPaddle, BotWhite)) {
-            RightPaddle.y += 20;
-        }
-        if (IsKeyDown(KEY_W) && !CheckCollisionRecs(LeftPaddle, TopWhite)) {
-            LeftPaddle.y -= 20;
-        }
-        if (IsKeyDown(KEY_S) && !CheckCollisionRecs(LeftPaddle, BotWhite)) {
-            LeftPaddle.y += 20;
-        }
-        if ((winRight || winLeft) && IsKeyPressed(KEY_SPACE)) {
-            winRight = false;
-            winLeft = false;
-        }
-
-        // Ball Movement
-        ballPos.x += ballSpeed.x;
-        ballPos.y += ballSpeed.y;
-        
-        // Checking Ball Collision
-
-        // White Walls
-        if (CheckCollisionCircleRec(ballPos, ballRadius, BotWhite)) {
-            ballSpeed.y *= -1;
-        }
-        if (CheckCollisionCircleRec(ballPos, ballRadius, TopWhite)) {
-            ballSpeed.y *= -1;
-        }
-
-        // Paddles
-        if (CheckCollisionCircleRec(ballPos, ballRadius, LeftPaddle)) {
-            ballSpeed.x *= -1;
-        }
-        if (CheckCollisionCircleRec(ballPos, ballRadius, RightPaddle)) {
-            ballSpeed.x *= -1;
-        }
-
-        // Red Walls
-        if (CheckCollisionCircleRec(ballPos, ballRadius, LeftRed) && !winRight) {
-            winRight = true;
-            score.right += 1;
-        }
-        if (CheckCollisionCircleRec(ballPos, ballRadius, RightRed) && !winLeft) {
-            winLeft = true;
-            score.left += 1;
-        }
 
         // Drawing on Screen
         BeginDrawing();
 
             ClearBackground(BLACK);
-
-            DrawRectangleRec(TopWhite, RAYWHITE);
-            DrawRectangleRec(BotWhite, RAYWHITE);
-            DrawRectangleRec(LeftRed, RED);
-            DrawRectangleRec(RightRed, RED);
-
-            if (!winRight && !winLeft) {
-                DrawRectangleRec(RightPaddle, RAYWHITE);
-                DrawRectangleRec(LeftPaddle, RAYWHITE);
-                DrawCircleV(ballPos, ballRadius, RAYWHITE);
-            }
-
-            if (winRight) {
-                ballPos.x = 526.25;
-                ballPos.y = 351.25; 
-                DrawText("Right Side Wins", 350, 335, 50, RAYWHITE);
-                DrawText("Press Space To Play Again", 400, 400, 20, RAYWHITE);
-                snprintf(scoreText, sizeof(scoreText), "Score\n  %d | %d", score.left, score.right);
-                DrawText(scoreText, 470, 100, 50, RAYWHITE);
-            }
-            if (winLeft) {
-                ballPos.x = 526.25;
-                ballPos.y = 351.25;
-                DrawText("Left Side Wins", 350, 335, 50, RAYWHITE);
-                DrawText("Press Space To Play Again", 400, 400, 20, RAYWHITE);
-                snprintf(scoreText, sizeof(scoreText), "Score\n  %d | %d", score.left, score.right);
-                DrawText(scoreText, 470, 100, 50, RAYWHITE);
-                
-            }
 
         EndDrawing();
     }
