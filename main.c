874c7826509cd948989a2d38382fbafbdba42223
PennacchioDavide <dapennacchio@hotmail.com>
2024-12-23 10:54:44 +0100

    basic structure

--- main.c
+++ main.c
@@ -1,37 +1,12 @@
 #include <raylib.h>
 #include <stdlib.h>
-#include <raymath.h>
-#include <time.h>
 
-#define COLS 10
-
-#define ROWS 10
-
-const int screenWidth = 400;
-const int screenHeight = 400;
-
-const int cellWidth = screenWidth / COLS;
-const int cellHeight= screenHeight / ROWS;
-
-typedef struct Cell {
-    int i;
-    int j;
-}Cell;
-
-Cell grid[ROWS][COLS];
+const int screenWidth = 1080;
+const int screenHeight = 720;
 
 int main() {
 
-    srand(time(0));
-
     InitWindow(screenWidth, screenHeight, "basic window"); // Window Initialisation
-
-    for (int i = 0; i < COLS; i++) {
-        for (int j = 0; j < ROWS; j++) {
-            grid[i][j].i = i;
-            grid[i][j].j = j;
-        }   
-    }
 
     while (!WindowShouldClose()) { // Game Loop
 
@@ -39,20 +14,10 @@ int main() {
 
             ClearBackground(RAYWHITE); // Background Color
 
-            for (int i = 0; i < COLS; i++) {
-                for (int j = 0; j < ROWS; j++) {
-                    CellDraw(grid[i][j]);
-                }   
-            }
-
         EndDrawing();
     }
 
     CloseWindow(); // WIndow Closing
 
     return 0;
-}
-
-void CellDraw(Cell cell) {
-    DrawRectangleLines(cell.i * cellWidth, cell.j * cellHeight, cellWidth, cellHeight, BLACK);
 }
